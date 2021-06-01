ea16267756b0707dfba8761d8ad69bf3ff3a95cf
Tim <tim7304@gmail.com>
2021-06-01 15:54:14 +0200

    training loop is more readable

--- code/train.py
+++ code/train.py
@@ -1,5 +1,5 @@
 """
-this file holds the model train loop
+this file holds the model training loop
 """
 
 import os
@@ -12,71 +12,44 @@ import visuals
 import dataloader
 import model
 
-
-# set the arguments
+# set the device, training/model arguments, init the experiment dir and init visual tools
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 args = options.get_args()
-
 img_save_path = os.path.join(args.name, 'images')
 os.makedirs(img_save_path, exist_ok=True)
-
-C,H,W = args.channels, args.img_size, args.img_size
-
-# Initialize the generator and discriminator
-generator = model.get_network(net_type='generator', channels=args.channels)
-discriminator = model.get_network(net_type='discriminator', channels=args.channels)
-
-# Load the data
-dataloader = dataloader.load_fashionMNIST(args, isTrain=True)
-
-# load visualization board
 writer = visuals.Writer(args.name)
 
-# Loss function
-adversarial_loss = torch.nn.BCELoss()
-
-# Optimizers
+# Initialize the networks, optimizers, loss function and data
+generator = model.get_network(net_type='generator', args=args).to(device)
+discriminator = model.get_network(net_type='discriminator', args=args).to(device)
+adversarial_loss = torch.nn.BCELoss().to(device)
 optimizer_G = torch.optim.Adam(generator.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
 optimizer_D = torch.optim.Adam(discriminator.parameters(), lr=args.lr, betas=(args.beta1, args.beta2))
-
-# set to CUDA, if available
-if torch.cuda.is_available():
-    generator.cuda()
-    discriminator.cuda()
-    adversarial_loss.cuda()
+dataloader = dataloader.load_fashionMNIST(args, isTrain=True)
 
 for epoch in range(args.n_epochs):
     for i, (imgs, labels) in enumerate(dataloader):
-
-        Batch_Size = args.batch_size
-        N_Class = args.n_classes
-        img_size = args.img_size
         # Adversarial ground truths
-        valid = Variable(torch.ones(Batch_Size).cuda(), requires_grad=False)
-        fake = Variable(torch.zeros(Batch_Size).cuda(), requires_grad=False)
+        valid = Variable(torch.ones(args.batch_size).to(device), requires_grad=False)
+        fake = Variable(torch.zeros(args.batch_size).to(device), requires_grad=False)
 
         # Configure input
-        real_imgs = Variable(imgs.type(torch.FloatTensor).cuda())
-
-        real_y = torch.zeros(Batch_Size, N_Class)
-        real_y = real_y.scatter_(1, labels.view(Batch_Size, 1), 1).view(Batch_Size, N_Class, 1, 1).contiguous()
-        real_y = Variable(real_y.expand(-1, -1, img_size, img_size).cuda())
+        real_imgs = Variable(imgs.type(torch.FloatTensor)).to(device)
+        labels = Variable(labels.type(torch.LongTensor)).to(device)
 
         # Sample noise and labels as generator input
-        noise = Variable(torch.randn((Batch_Size, args.latent_dim,1,1)).cuda())
-        gen_labels = (torch.rand(Batch_Size, 1) * N_Class).type(torch.LongTensor)
-        gen_y = torch.zeros(Batch_Size, N_Class)
-        gen_y = Variable(gen_y.scatter_(1, gen_labels.view(Batch_Size, 1), 1).view(Batch_Size, N_Class,1,1).cuda())
+        noise = Variable(torch.randn((args.batch_size, args.latent_dim, 1, 1)).to(device))
+        gen_labels = Variable(torch.LongTensor(np.random.randint(0, args.n_classes, args.batch_size))).to(device)
+
         # ---------------------
         #  Train Discriminator
         # ---------------------
         optimizer_D.zero_grad()
         # Loss for real images
-        d_real_loss = adversarial_loss(discriminator(real_imgs, real_y).squeeze(), valid)
+        d_real_loss = adversarial_loss(discriminator(real_imgs, labels).squeeze(), valid)
         # Loss for fake images
-        gen_imgs = generator(noise, gen_y)
-        gen_y_for_D = gen_y.view(Batch_Size, N_Class, 1, 1).contiguous().expand(-1, -1, img_size, img_size)
-
-        d_fake_loss = adversarial_loss(discriminator(gen_imgs.detach(),gen_y_for_D).squeeze(), fake)
+        gen_imgs = generator(noise, gen_labels)
+        d_fake_loss = adversarial_loss(discriminator(gen_imgs.detach(),gen_labels).squeeze(), fake)
         # Total discriminator loss
         d_loss = (d_real_loss + d_fake_loss)
         d_loss.backward()
@@ -85,22 +58,19 @@ for epoch in range(args.n_epochs):
         # -----------------
         #  Train Generator
         # -----------------
-
         optimizer_G.zero_grad()
-
-        g_loss = adversarial_loss(discriminator(gen_imgs,gen_y_for_D).squeeze(), valid)
+        # generator loss
+        g_loss = adversarial_loss(discriminator(gen_imgs, gen_labels).squeeze(), valid)
         g_loss.backward()
         optimizer_G.step()
 
+        # visualization, user output and model saving
         batches_done = epoch * len(dataloader) + i
         if batches_done % args.sample_interval == 0:
-            noise = Variable(torch.FloatTensor(np.random.normal(0, 1, (N_Class**2, args.latent_dim,1,1))).cuda())
-            #fixed labels
-            y_ = torch.LongTensor(np.array([num for num in range(N_Class)])).view(N_Class,1).expand(-1,N_Class).contiguous()
-            y_fixed = torch.zeros(N_Class**2, N_Class)
-            y_fixed = Variable(y_fixed.scatter_(1,y_.view(N_Class**2,1),1).view(N_Class**2, N_Class,1,1).cuda())
-
-            gen_imgs = generator(noise, y_fixed).view(-1,C,H,W)
+            noise = Variable(torch.FloatTensor(np.random.normal(0, 1, (args.n_classes**2, args.latent_dim,1,1))).to(device))
+            labels = np.array([num for _ in range(args.n_classes) for num in range(args.n_classes)])
+            y_labels = Variable(torch.LongTensor(labels)).to(device)
+            gen_imgs = generator(noise, y_labels).view(-1, args.channels, args.img_size, args.img_size)
 
             visuals.save_im(gen_imgs, img_save_path, epoch, batches_done, args.n_classes)
 
@@ -110,7 +80,9 @@ for epoch in range(args.n_epochs):
             writer.add_losses(g_loss, d_loss, batches_done)
 
         if epoch > 0 and epoch % args.save_interval == 0:
-            torch.save(generator.state_dict(), os.path.join(args.name, 'epoch_%d.pth' % epoch))
+            torch.save(generator.state_dict(), os.path.join(args.name, 'gen_epoch_%d.pth' % epoch))
+            torch.save(discriminator.state_dict(), os.path.join(args.name, 'dis_epoch_%d.pth' % epoch))
 
 writer.close_writer()
 torch.save(generator.state_dict(), os.path.join(args.name, 'final_trained_generator.pth'))
+torch.save(discriminator.state_dict(), os.path.join(args.name, 'final_trained_discriminator.pth'))
